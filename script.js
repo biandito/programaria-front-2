5b9d197c9ab8b36b669d0686a35fab1288978a5a
biandito <bekatrivz@gmail.com>
2023-06-21 11:34:28 -0300

    update 3

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregaDicionario() {
+
+    var biografias = [
+  
+      "Marielle Franco",
+  
+      "Rosa Parks",
+  
+      "Angela Davis",
+  
+      "Carolina de Jesus",
+  
+      "Lélia Gonzalez",
+  
+      "Conceição Evaristo",
+  
+    ];
+  
+    alert(biografias);
+  
+  }
+  
+  carregaDicionario();
